bafdd79bd7f7e6dcd4b081c278b1cac6d847163c
Ashwini Ravi Nair <itsmeashwini1104@gmail.com>
2020-03-13 21:03:02 +0530

    Updates

---
+++ precache-manifest.c4aab5a07b8bdd0c46b3644cf3abdd75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec0e3770b2d2ac849060b8558002be31",
+    "url": "/drag-and-draw-bounding-box-on-image-react/index.html"
+  },
+  {
+    "revision": "09ce530fca69e8a8dce6",
+    "url": "/drag-and-draw-bounding-box-on-image-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0ec4c29ef04487ab0bae",
+    "url": "/drag-and-draw-bounding-box-on-image-react/static/css/main.06a00d3e.chunk.css"
+  },
+  {
+    "revision": "09ce530fca69e8a8dce6",
+    "url": "/drag-and-draw-bounding-box-on-image-react/static/js/2.0dc3cc1b.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/drag-and-draw-bounding-box-on-image-react/static/js/2.0dc3cc1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c717d361994065920a64",
+    "url": "/drag-and-draw-bounding-box-on-image-react/static/js/3.4af90d25.chunk.js"
+  },
+  {
+    "revision": "0ec4c29ef04487ab0bae",
+    "url": "/drag-and-draw-bounding-box-on-image-react/static/js/main.3c8f6c5c.chunk.js"
+  },
+  {
+    "revision": "08f2f8e7342b00d88e0b",
+    "url": "/drag-and-draw-bounding-box-on-image-react/static/js/runtime-main.97ad1661.js"
+  }
+]);
